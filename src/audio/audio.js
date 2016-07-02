f8fe5b7ac95cdca69c16efe600836a8972844dae
Amir Eldor <amir.eldor@gmail.com>
2016-07-03 01:54:05 +0300

    Yay reversed phase is working using AudioBufferManager

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -6,9 +6,67 @@
 const audioContext = typeof window !== 'undefined' &&
         new (window.AudioContext || window.webkitAudioContext)();
 
-let bufferSource;
+/**
+ * Some abstraction. Manages the main audio buffer.
+ */
+class AudioBufferManager {
+  constructor() {
+    if (!audioContext) return;
+    this.scriptProcessorNode = audioContext.createScriptProcessor();
+    // Use deprecated method as new method is not widely implemented
+    this.scriptProcessorNode.onaudioprocess = this.onAudioProcess;
+    this.scriptProcessorNode.connect(audioContext.destination);
+    this.bufferSourceNode = null;
+  }
 
-let audioBuffer;  // Current audio buffer data
+  onAudioProcess(audioProcessingEvent) {
+    const inputBuffer = audioProcessingEvent.inputBuffer;
+    const outputBuffer = audioProcessingEvent.outputBuffer;
+
+    const inputLeft = inputBuffer.getChannelData(0);
+    const inputRight = inputBuffer.getChannelData(1);
+    const outputLeft = outputBuffer.getChannelData(0);
+    const outputRight = outputBuffer.getChannelData(1);
+
+    for (let sample = 0; sample < inputLeft.length; sample++) {
+      outputLeft[sample] = inputLeft[sample] - inputRight[sample];
+      outputRight[sample] = inputRight[sample] - inputLeft[sample];
+    }
+  }
+
+  setBuffer(audioBuffer) {
+    // TODO: check for stereo
+    this.clear();
+    this.bufferSourceNode = audioContext.createBufferSource();
+    this.bufferSourceNode.buffer = audioBuffer;
+    this.bufferSourceNode.connect(this.scriptProcessorNode);
+  }
+
+  play() {
+    if (!this.bufferSourceNode) {
+      console.error('AudioBufferManager: no buffer to play');
+      return;
+    }
+    this.bufferSourceNode.start();
+  }
+
+  pause() {
+    if (!this.bufferSourceNode) {
+      console.error('AudioBufferManager: no buffer to pause');
+      return;
+    }
+    this.bufferSourceNode.stop();
+  }
+
+  clear() {
+    if (this.bufferSourceNode) {
+      this.bufferSourceNode.disconnect();
+    }
+    this.bufferSourceNode = null;
+  }
+}
+
+const audioBufferManager = new AudioBufferManager();
 
 export async function loadFile(file) {
   const data = await new Promise((resolve, reject) => {
@@ -19,24 +77,18 @@ export async function loadFile(file) {
   });
   return new Promise((resolve) => {
     audioContext.decodeAudioData(data, (buffer) => {
-      audioBuffer = buffer;
-      if (bufferSource) {
-        bufferSource.disconnect();
-      }
-      bufferSource = audioContext.createBufferSource();
-      bufferSource.buffer = audioBuffer;
-      bufferSource.connect(audioContext.destination);
-      resolve(bufferSource);
+      audioBufferManager.setBuffer(buffer);
+      resolve(buffer);
     });
   });
 }
 
 export function play() {
   console.log('audio play');
-  bufferSource.start();
+  audioBufferManager.play();
 }
 
 export function pause() {
   console.log('audio pause');
-  bufferSource.stop();
+  audioBufferManager.stop();
 }
