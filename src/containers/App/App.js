579d65485489776d990cdac7db7bd29f394aef2f
Amir Eldor <amir.eldor@gmail.com>
2016-07-02 16:21:58 +0300

    Strip, strip everything! Now there's only "Hi" on the page

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { IndexLink } from 'react-router';
+// import { IndexLink } from 'react-router';
 // import { LinkContainer } from 'react-router-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
+// import Navbar from 'react-bootstrap/lib/Navbar';
 // import Nav from 'react-bootstrap/lib/Nav';
 // import NavItem from 'react-bootstrap/lib/NavItem';
 import Helmet from 'react-helmet';
 import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
 // import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
-import { InfoBar } from 'components';
+// import { InfoBar } from 'components';
 import { push } from 'react-router-redux';
 import config from '../../config';
 import { asyncConnect } from 'redux-async-connect';
@@ -70,6 +70,7 @@ export default class App extends Component {
     return (
       <div className={styles.app}>
         <Helmet {...config.app.head}/>
+        {/*
         <Navbar fixedTop>
           <Navbar.Header>
             <Navbar.Brand>
@@ -80,7 +81,6 @@ export default class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle/>
           </Navbar.Header>
-          {/*
           <Navbar.Collapse eventKey={0}>
             <Nav navbar>
               {user && <LinkContainer to="/chat">
@@ -116,20 +116,13 @@ export default class App extends Component {
               </NavItem>
             </Nav>
           </Navbar.Collapse>
-          */}
         </Navbar>
+        */}
 
         <div className={styles.appContent}>
           {this.props.children}
         </div>
-        <InfoBar/>
 
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
-        </div>
       </div>
     );
   }
