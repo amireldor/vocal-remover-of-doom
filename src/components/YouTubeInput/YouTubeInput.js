a4f5b105a27a89cd1a914f628304d35e369c437d
Amir Eldor <amir.eldor@gmail.com>
2016-07-05 01:06:18 +0300

    lodash + adding debounced search youtube dummy function

--- src/components/YouTubeInput/YouTubeInput.js
+++ src/components/YouTubeInput/YouTubeInput.js
@@ -1,8 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import _ from 'lodash';  // eslint-disable-line id-length
 
 import * as songActions from 'redux/modules/song';
+
+const SEARCH_WAIT_TIME = 457;  // Milliseconds
 
 @connect(
   state => ({}),  // eslint-disable-line no-unused-vars <-- TMEPORARY
@@ -16,12 +19,19 @@ class PlayerControls extends Component {
     this.state = {
       searchTerm: ''
     };
+    // A function to start searching YouTube after some text was entered without flooding stuff
+    this.startYouTubeSearch = _.debounce(this._startYouTubeSearch, SEARCH_WAIT_TIME);
   }
 
   onInputBoxChange(event) {
     const text = event.target.value;
-    console.log(text);
     this.setState({ searchTerm: text });
+    this.startYouTubeSearch(text);
+  }
+
+  // Don't call this! Call it without the '_'
+  _startYouTubeSearch(searchTerm) {
+    console.log('YOTUBE:', searchTerm);
   }
 
   render() {
